13425c80887c4747f15ffc6fd3fc7d4adde7b85c
antor <antor.mahmud96@gmail.com>
2019-07-23 00:27:04 +0600

    working all

--- device/cripto/views.py
+++ device/cripto/views.py
@@ -5,43 +5,9 @@ from django.http import HttpResponse, HttpResponseRedirect
 from rest_framework import status
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
-from cripto.models import Self
+from cripto.models import Self, Miner
 from cripto.serializers import SelfSerializer, PartialSerializer
 import requests
-
-
-@api_view(['GET', 'POST'])
-def partial(request):
-    self = Self()
-    key1=5
-    device = DH_Endpoint(self.public_key, key1, self.private_key)
-    partial_key = device.generate_partial_key()
-    print(partial_key)
-    return HttpResponse('This is the partial')
-
-
-
-def td(request):
-    return HttpResponseRedirect('http://127.0.0.1:8000/users/me/')
-
-
-
-# @api_view(['GET', 'POST'])
-# def cripto_list(request, format=None):
-#     """
-#     List all code snippets, or create a new snippet.
-#     """
-#     if request.method == 'GET':
-#         criptos = Self.objects.all()
-#         serializer = SelfSerializer(criptos, many=True)
-#         return Response(serializer.data)
-#
-#     elif request.method == 'POST':
-#         serializer = SelfSerializer(data=request.data)
-#         if serializer.is_valid():
-#             serializer.save()
-#             return Response(serializer.data, status=status.HTTP_201_CREATED)
-#         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
 
@@ -58,9 +24,18 @@ def cripto_detail(request, pk):
     serializer = PartialSerializer(cripto)
 
 
-    # partial = requests.get('http://127.0.0.1:8000/device/partial/7919')
-    # par = partial.json()
-    # print(par)
 
+    miner_per = requests.get('http://127.0.0.1:8000/device/partial/7919')
+    par = miner_per.json()
+    print(par)
+    device = Self.objects.get(public_key=7919)
+    device.partial = par['partial']
+    device.save()
+    med = DH_Endpoint(device.public_key, 19, device.private_key)
+    device.dh_key = med.generate_full_key(device.partial)
+    device.save()
 
     return Response(serializer.data)
+
+
+
