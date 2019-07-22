673d9a48d2ab85e1719259cf18e61c690606bf1e
antor <antor.mahmud96@gmail.com>
2019-07-23 00:23:14 +0600

    working deffie hellmen

--- src/device/urls.py
+++ src/device/urls.py
@@ -3,10 +3,8 @@ from rest_framework.urlpatterns import format_suffix_patterns
 from device import views
 
 urlpatterns = [
-    path('', views.device_list),
     path('partial/<int:pk>', views.partial),
-    path('<int:pk>', views.device_detail),
-    path('try/', views.cripto_connect),
+    path('', views.cripto_connect),
 ]
 
 urlpatterns = format_suffix_patterns(urlpatterns)
